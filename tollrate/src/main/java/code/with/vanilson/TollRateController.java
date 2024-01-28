ccc69003fe2065e6c8ce94da1ca530342799c9f8
edsonwade <vanilsonmuhongo@gamil.com>
2024-01-28 20:36:08 +0000

    TollRate UI is done. do the same with fastpass.

--- tollrate/src/main/java/code/with/vanilson/TollRateController.java
+++ tollrate/src/main/java/code/with/vanilson/TollRateController.java
@@ -6,36 +6,37 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.Instant;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
 @RestController
-@RequestMapping(path = "/api/v1/toll-rates")
 public class TollRateController {
 
-    List<TollRate> tollRates = List.of(
-            new TollRate(1, 5.6f,
-                    "2024-01-28T12:48:56.27"),
-            new TollRate(2, 3.4f,
-                    "2024-01-28T12:49:57.29"),
-            new TollRate(3, 45.4f,
-                    "2024-01-29T12:49:57.29"),
-            new TollRate(4, 3.4f,
-                    "2024-01-30T12:49:57.29")
-    );
+    List<TollRate> tollRates;
 
-    @GetMapping
+    public TollRateController() {
+
+        tollRates = new ArrayList<>();
+        tollRates.add(new TollRate(1000, 0.55f, Instant.now().toString()));
+        tollRates.add(new TollRate(1001, 1.05f, Instant.now().toString()));
+        tollRates.add(new TollRate(1002, 0.60f, Instant.now().toString()));
+        tollRates.add(new TollRate(1003, 1.00f, Instant.now().toString()));
+
+    }
+
+    @GetMapping(value = "tollrate")
     public ResponseEntity<List<TollRate>> listAllRates() {
         return ResponseEntity.ok(Collections.unmodifiableList(tollRates));
     }
 
-    @GetMapping(value = "/{stationId}")
-    public ResponseEntity<TollRate> listRatesById(@PathVariable(name = "stationId") Integer stationId) {
+    @RequestMapping(value = "/tollrate/{stationId}")
+    public ResponseEntity<TollRate> listRatesById(@PathVariable int stationId) {
         var tollRate = tollRates.stream()
-                .filter(rate -> stationId.equals(rate.getStationId()))
-                .findFirst()
-                .orElseThrow(
-                        () -> new TollRateByIdNotFoundException(" rate with id " + stationId + " was not founded"));
+                .filter(rate -> stationId == rate.getStationId())
+                .findAny()
+                .orElse(new TollRate());
         return ResponseEntity.ok().body(tollRate);
 
     }
