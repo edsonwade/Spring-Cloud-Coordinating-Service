e345bd63d17a186ffbc089a50eac39a04c018101
edsonwade <vanilsonmuhongo@gamil.com>
2024-01-30 13:34:52 +0000

    circuit breaker done

--- tollrate-ui/src/main/java/code/with/vanilson/TollRateUIController.java
+++ tollrate-ui/src/main/java/code/with/vanilson/TollRateUIController.java
@@ -1,34 +1,47 @@
 package code.with.vanilson;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.cloud.client.circuitbreaker.ReactiveCircuitBreakerFactory;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
 
 @Controller
 
 @Slf4j
 public class TollRateUIController {
     private final WebClient.Builder builder;
+    private final ReactiveCircuitBreakerFactory circuitBreakerFactory;
 
-    public TollRateUIController(WebClient.Builder builder) {
+    public TollRateUIController(WebClient.Builder builder, ReactiveCircuitBreakerFactory circuitBreakerFactory) {
         this.builder = builder;
+        this.circuitBreakerFactory = circuitBreakerFactory;
     }
 
     @RequestMapping("/dashboard")
     public String getTollRate(@RequestParam(defaultValue = "1000") Integer stationId, Model m) {
 
-        TollRate rate = builder.build().get()
-                .uri("http://tollrate-service/tollrate/" + stationId)
-                .retrieve()
-                .bodyToMono(TollRate.class)
-                .block();
+        var rcb = circuitBreakerFactory.create("tollrate-cb");
+
+        Mono<TollRate> rate = rcb.run(builder.build().get()
+                        .uri("http://tollrate-service/tollrate/" + stationId)
+                        .retrieve()
+                        .bodyToMono(TollRate.class),
+                // throw exception if the call fails
+                throwable -> getDefaultRate());
+
         log.info("stationId {}: ", stationId);
-        m.addAttribute("rate", rate);
+        m.addAttribute("rate", rate.block());
         return "dashboard";
+    }
+
+    private Mono<TollRate> getDefaultRate() {
+        log.info("Fallback method called");
+        return Mono.just(new TollRate(0, 2.00f, ""));
     }
 }
